374f9044eaf2c7fc0d445110f684236c10ca6a0c
Alex Lushpai <alex@lushpai.org>
2013-06-18 18:04:03 +0400

    debug messages

--- api.pl
+++ api.pl
@@ -9,13 +9,13 @@ get '/' => {text => "Who's there?!"};
 get '/*domain/queue' => sub {
     my $self   = shift;
     my $domain = $self->param('domain');
-    my $db = DBI->connect("dbi:SQLite:dbname=db/queue.db","","") or app->log->debug('Starting application.');
+    my $db = DBI->connect("dbi:SQLite:dbname=db/queue.db","","") or app->log->debug($!);
     $db->{sqlite_unicode} = 1;
-    my $query = $db->do("INSERT INTO domain (domain, status) VALUES('$domain', 0)") or app->log->debug('Starting application.');
+    my $query = $db->do("INSERT INTO domain (domain, status) VALUES('$domain', 0)") or app->log->debug($!);
     $query > 0
       ? $self->render(json => {"status" => "success", "message" => "$domain placed in queue"})
       : $self->render(json => {"status" => "error", "message" => "queue crushed"});
 };
 
-
 app->start;
+
