fb3f93592e6e012d335854d7b1ead9c7f40e1380
Alex Lushpai <alex@lushpai.org>
2013-06-21 17:56:38 +0400

    Update generate.pl

--- generate.pl
+++ generate.pl
@@ -5,41 +5,65 @@ use utf8;
 use DBD::SQLite;
 use File::Copy;
 
-my $conf = require 'config/bzgen.conf';
-my $db = DBI->connect("dbi:SQLite:dbname=db/queue.db","","");
-my $query = $db->prepare("SELECT * FROM domain WHERE (status == 0)");
+our $conf = require 'config/bzgen.conf';
 
-$query->execute() or die($db->errstr);
-
-open LOG, ">>", 'log/cron.log' or die $!;
-print LOG "================= start task ================\n\n";
-
-while (my $row = $query->fetchrow_arrayref()) {
-  open FILE, "<templates/template.$conf->{'servtype'}" or die $!;
-  open CONF, ">>", $conf->{'named_path'} or die $!;
-  my @lines = <FILE>;
-
-  foreach my $i (@lines) {
-    if ($i =~ /__domain__/gi) {
-      $i =~ s/__domain__/@$row[1]/;
-    }
-    print CONF $i;
-  }
-
-  close CONF or die $!; close FILE or die $!;
-
-  if ($conf->{'servtype'} =~ /master/) {
-    copy('templates/template.zone', "$conf->{'zones_path'}/@$row[1].zone") or die $!;
-  }
-
-  my $q = $db->prepare("UPDATE domain SET status = 1 WHERE (id == @$row[0])");
-  $q->execute() or die($db->errstr);
-
-  print LOG "@$row[1] added";
-
-}
-
-print LOG "================= end task ================\n\n";
-close LOG or die $!;
+&domain_queue;
+&cname_queue if ($conf->{'servtype'} =~ /master/);
 system("rndc", "reload");
+
+sub domain_queue {
+  my $db = DBI->connect("dbi:SQLite:dbname=db/queue.db","","");
+  my $query = $db->prepare("SELECT * FROM domain WHERE (status == 0)");
+
+  $query->execute() or die($db->errstr);
+
+  open LOG, ">>", 'log/cron.log';
+
+  while (my $row = $query->fetchrow_arrayref()) {
+    open FILE, "<templates/template.$conf->{'servtype'}";
+    open CONF, ">>", $conf->{'named_path'};
+    my @lines = <FILE>;
+
+    foreach my $i (@lines) {
+      if ($i =~ /__domain__/gi) {
+        $i =~ s/__domain__/@$row[1]/;
+      }
+      print CONF $i;
+    }
+
+    close CONF; close FILE;
+
+    if ($conf->{'servtype'} =~ /master/) {
+      copy('templates/template.zone', "$conf->{'zones_path'}/@$row[1].zone");
+    }
+
+    my $q = $db->prepare("UPDATE domain SET status = 1 WHERE (id == @$row[0])");
+    $q->execute() or die($db->errstr);
+
+    print LOG "zone for @$row[1] added\n";
+
+  }
+
+  close LOG;
+};
+
+sub cname_queue {
+  my $db = DBI->connect("dbi:SQLite:dbname=db/queue.db","","");
+  my $query = $db->prepare("SELECT * FROM cname WHERE (status == 0)");
+
+  $query->execute() or die($db->errstr);
+
+  open LOG, ">>", 'log/cron.log';
+
+  while (my $row = $query->fetchrow_arrayref()) {
+    open ZONE, ">>", $conf->{'zones_path'} . '/' . @$row[1] . '.zone';
+    print ZONE @$row[2] . "\tIN CNAME\tmail.yandex.ru.";
+    close ZONE;
+    my $q = $db->prepare("UPDATE cname SET status = 1 WHERE (id == @$row[0])");
+    $q->execute() or die($db->errstr);
+    print LOG "cname for @$row[1] added\n";
+  }
+
+  close LOG;
+};
 
