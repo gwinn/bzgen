3a9e09bf5ae5ce1f0cf7e3c85c3eb0d79f885bdb
Alex Lushpai <alex@lushpai.org>
2013-07-18 10:45:44 +0400

    add api key

--- api.pl
+++ api.pl
@@ -4,36 +4,50 @@ use utf8;
 use Mojolicious::Lite;
 use DBD::SQLite;
 
+our $conf = require 'config/bzgen.conf';
+
 get '/' => {text => "Who's there?!"};
 
-get '/*domain/queue' => sub {
+get '/*domain/*key/queue' => sub {
     my $self   = shift;
     my $domain = $self->param('domain');
+    my $key    = $self->param('key');
 
-    my $db = DBI->connect("dbi:SQLite:dbname=db/queue.db","","") or app->log->debug($!);
-    $db->{sqlite_unicode} = 1;
-    my $query = $db->do("INSERT INTO domain (domain, status) VALUES('$domain', 0)") or app->log->debug($!);
+    if $conf->{'apikey'} != $key {
+      $self->render(json => {"status" => "error", "message" => "bad api key"});
+    } else {
 
-    $query > 0
-      ? $self->render(json => {"status" => "success", "message" => "$domain placed in queue"})
-      : $self->render(json => {"status" => "error", "message" => "queue crushed"});
+      my $db = DBI->connect("dbi:SQLite:dbname=db/queue.db","","") or app->log->debug($!);
+      $db->{sqlite_unicode} = 1;
+      my $query = $db->do("INSERT INTO domain (domain, status) VALUES('$domain', 0)") or app->log->debug($!);
+
+      $query > 0
+        ? $self->render(json => {"status" => "success", "message" => "$domain placed in queue"})
+        : $self->render(json => {"status" => "error", "message" => "queue crushed"});
+    }
 };
 
-get '/*domain/*cname/update_cname' => sub {
+get '/*domain/*cname/*key/update_cname' => sub {
     my $self   = shift;
     my $domain = $self->param('domain');
     my $cname  = $self->param('cname');
+    my $key    = $self->param('key');
 
-    app->log->debug($domain);
-    app->log->debug($cname);
+    if $conf->{'apikey'} != $key {
+      $self->render(json => {"status" => "error", "message" => "bad api key"});
+    } else {
 
-    my $db = DBI->connect("dbi:SQLite:dbname=db/queue.db","","") or app->log->debug("Connection error: " . $!);
-    $db->{sqlite_unicode} = 1;
-    my $query = $db->do("INSERT INTO cname (domain, cname, status) VALUES('$domain', '$cname', 0)") or app->log->debug("Query error: " . $!);
+      app->log->debug($domain);
+      app->log->debug($cname);
 
-    $query > 0
-      ? $self->render(json => {"status" => "success", "message" => "cname record $cname for $domain placed in queue"})
-      : $self->render(json => {"status" => "error", "message" => "queue crushed"});
+      my $db = DBI->connect("dbi:SQLite:dbname=db/queue.db","","") or app->log->debug("Connection error: " . $!);
+      $db->{sqlite_unicode} = 1;
+      my $query = $db->do("INSERT INTO cname (domain, cname, status) VALUES('$domain', '$cname', 0)") or app->log->debug("Query error: " . $!);
+
+      $query > 0
+        ? $self->render(json => {"status" => "success", "message" => "cname record $cname for $domain placed in queue"})
+        : $self->render(json => {"status" => "error", "message" => "queue crushed"});
+    }
 };
 
 app->start;
